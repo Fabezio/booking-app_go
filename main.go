0735d388cc975c2b5d579998c6a015f5d2d2b771
fabezio <fabezio@outlook.fr>
2022-04-25 15:42:28 +0200

    séparation des blocs

--- main.go
+++ main.go
@@ -7,16 +7,17 @@ import (
 )
 
 const conferenceName string = "Go Conference"
-var conferenceTickets uint = 50
-var remainingTokens = conferenceTickets
+var conferenceTokens uint = 50
+var remainingTokens = conferenceTokens
 // var userTokens
 var bookings []string
+// firstNames := []string{}
 // var addBooking = true
 // var answer = ""
 
 func main () {
-	fmt.Printf("Bienvenue sur notre application de réservation %v\n",conferenceName)
-	fmt.Println("Obtenez vos jetons pour assister à l'événement")
+	greetUsers(conferenceName, conferenceTokens)
+
 	for remainingTokens > 0 && len(bookings) < 50 {
 		var firstName string
 		var lastName string
@@ -36,16 +37,10 @@ func main () {
 		
 		isValidName := len(firstName) >= 2 && len(lastName) >= 2
 		isValidEmail := strings.Contains(email, "@")
-		if !isValidName {
-			fmt.Println("Attention: votre nom ou prénom doit contenir au minimum deux caractères")
-			continue
-		}
-		if !isValidEmail {
-			fmt.Println("Attention: votre adresse email doit contenir le symbole \"@\"")
-			continue
-		}
+		isValidTokenNumber := tokensToDeal > 0 && tokensToDeal <= remainingTokens
+		
 
-		if tokensToDeal <= remainingTokens {
+		if isValidEmail && isValidName && isValidTokenNumber {
 			remainingTokens = remainingTokens - tokensToDeal
 			var fullName = firstName + " " + lastName
 			bookings = append(bookings, fullName)
@@ -53,23 +48,46 @@ func main () {
 		fmt.Printf("Merci, %v. Vous serez notifié sous peu par courriel à l'adresse %v. Passez une excellente journée.\n", firstName, email)
 			fmt.Println("----------")
 		
-			firstNames := []string{}
-			for _, booking := range bookings {
-				names := strings.Fields(booking)
-				firstNames = append(firstNames, names[0])
-			}
+			firstNames := printFirstNames(bookings) 
 			fmt.Printf("Participants: %v\n", firstNames)
+
 			if remainingTokens == 0 {
 				fmt.Println("Jetons épuisés. Nous espérons vous voir lors de la prochaine conférence.")
 				break
 			}
 		} else {
-			fmt.Println("Le nombre de jetons demandé est trop important. Nous espérons vous voir l'an prochain.")
-			fmt.Println("----------")
+			fmt.Println("/!\\")
+			if !isValidName {
+				fmt.Println("Attention: votre nom ou prénom doit contenir au minimum deux caractères")
+				// continue
+			}
+			if !isValidEmail {
+				fmt.Println("Attention: votre adresse email doit contenir le symbole \"@\"")
+				// continue
+			}
+			if !isValidTokenNumber {
+
+				fmt.Println("Le nombre de jetons demandé est trop important. Nous espérons vous voir l'an prochain.")
+			}
+			// fmt.Println("Euh... Y'a un problème, là!")
+			// fmt.Println("----------")
 			// continue
 		}
-
-
-		
 	} 
+}
+
+func greetUsers(confName string, tokens uint) {
+	fmt.Printf("Bienvenue sur notre application de réservation %v.\n",confName)
+	fmt.Printf("Il y a actuellement %v jetons disponibles.\n", tokens)
+	fmt.Println("Obtenez vos jetons pour assister à la représentation.")
+}
+
+func printFirstNames(obj []string) []string {
+		firstNames := []string{}
+			for _, booking := range obj {
+				names := strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			return firstNames
+			
 }
